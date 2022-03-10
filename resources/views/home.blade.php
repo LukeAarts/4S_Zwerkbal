32e557a9c52aa3d84a30d337ca0f3e7bc88f371c
LukeAarts <d280114@edu.rocwb.nl>
2022-03-10 12:34:09 +0100

    Changelog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,12 @@
 
 @section('content')
 
-    <h1>Test</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, eligendi porro! Asperiores odio deleniti voluptatibus obcaecati corporis tempora quos numquam molestiae! Dolorum quis hic doloribus debitis nesciunt minima, quisquam optio.</p>
+    <h1>Dashboard</h1>
+    <p>Zwerkbal (Engels: Quidditch) is een sport voor tovenaars uit de Harry Potterboeken van J.K. Rowling. In de wereld van Harry Potter is Zwerkbal een populaire sport: veel mensen praten erover mee en zijn zeer enthousiast als er een groot toernooi wordt gespeeld. Ook heeft iedere heks of tovenaar wel een favoriet Zwerkbalteam. Het idee van Zwerkbal lijkt te zijn gebaseerd op het non-fictieve horseball.</p>
+    <div class="container">
+    <div class="griditem">Aantal aankomende toernooien:<br><b>26</b></div>
+    <div class="image"><img src="/img/snitch.png"></div>
+    <div class="griditem">Totaal aantal teams:<br><b>5</b></div>
+    </div>
 
 @endsection
